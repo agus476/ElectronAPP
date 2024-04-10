cac08a0894533abcf89cd8ee3a3964087a9e2f36
Agustin <aguscpl10@gmail.com>
2024-04-10 11:13:45 -0300

    Primer commit sin agregado de correciones

---
+++ renderer.js
@@ -0,0 +1,83 @@
+const XLSX = require('xlsx');
+const path = require('path');
+
+
+const excelFilePath = path.join(__dirname, './MARCA X PROVEEDOR.xlsx');
+
+function procesarDatos(data) {
+    const proveedores = data[0].slice(1); // Obtener los nombres de los proveedores
+    const marcas = [];
+
+    for (let i = 1; i < data.length; i++) {
+        const marca = { Marca: data[i][0], Proveedores: {} };
+
+        for (let j = 1; j < data[i].length; j++) {
+            marca.Proveedores[proveedores[j - 1]] = data[i][j];
+        }
+
+        marcas.push(marca);
+    }
+
+    return marcas;
+}
+
+function buscarProveedorPrincipal(marca) {
+    const workbook = XLSX.readFile(excelFilePath);
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+    const marcasProcesadas = procesarDatos(data);
+
+    const marcaEncontrada = marcasProcesadas.find(m => m.Marca === marca);
+    if (!marcaEncontrada) {
+        return "Marca no encontrada"; // Marca no encontrada
+    }
+
+    const proveedorPrincipal = Object.entries(marcaEncontrada.Proveedores).find(([_, tipo]) => tipo === 'P');
+    if (!proveedorPrincipal) {
+        return "Proveedor principal no encontrado"; // Proveedor principal no encontrado
+    }
+    
+    const proveedoresSecundarios = Object.entries(marcaEncontrada.Proveedores)
+  .filter(([_, tipo]) => tipo === 'S')
+  .map(([proveedor, _]) => proveedor);
+  
+
+    if(!proveedoresSecundarios){
+        return `Lo puedes comprar en: ${proveedorPrincipal[0]}`;
+    }
+
+    else{
+        return `Principalmente se compra en ${proveedorPrincipal[0]}, pero tambien lo podes comprar en ${proveedoresSecundarios} `
+    }
+   
+
+    
+}
+
+function buscarMarcasPorProveedor(proveedor) {
+    const workbook = XLSX.readFile(excelFilePath);
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+    const proveedorIndex = data[0].indexOf(proveedor);
+    if (proveedorIndex === -1) {
+        return null; // Proveedor no encontrado
+    }
+
+    const marcas = [];
+    for (let i = 1; i < data.length; i++) {
+        if (data[i][proveedorIndex] === 'P' || data[i][proveedorIndex] === 'S') {
+            marcas.push(data[i][0]);
+        }
+    }
+
+    return marcas;
+}
+
+// Ejemplo de uso
+const proveedor = 'ALFA RODAMIENTOS SRL';
+const marcas = buscarMarcasPorProveedor(proveedor);
+console.log(`Las marcas asociadas al proveedor ${proveedor} son: ${marcas}`);
